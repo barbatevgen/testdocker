c273f98911ca4a5ba6216dfb815baa3163c75e28
ebarbat <barbatevgen@gmail.com>
2022-09-04 19:28:57 +0100

    first chenges in index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello frof kubernetes</h1><br>";
 echo "Server IP adress is : $ip_server","<br><p>";
-echo "spizgeno <front color=blue>";
+echo "spizgeno dvagdi <front color=blue>";
 
 ?>
