cc4dd430b8c6d51d7035e18bd55e9ec06243cda0
ebarbat <barbatevgen@gmail.com>
2022-09-04 19:37:28 +0100

    second chenges in index.php

--- index.php
+++ index.php
@@ -5,4 +5,6 @@ echo "<h1>Hello frof kubernetes</h1><br>";
 echo "Server IP adress is : $ip_server","<br><p>";
 echo "spizgeno dvagdi <front color=blue>";
 
+echo "producted EB <front color=red>";
+
 ?>
